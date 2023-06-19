b28b5d25d5394b571c087a3305b88fee1751e88b
Vitória <gitgitt0@gmail.com>
2023-06-19 20:13:11 -0300

    Viagens1commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarComponent } from './listar/listar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { DetalharComponent } from './detalhar/detalhar.component';
+import { ExcluirComponent } from './excluir/excluir.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'/listar', component:ListarComponent},
+  {path:'/cadastrar', component:CadastrarComponent},
+  {path:'/detalhar', component:DetalharComponent},
+  {path:'/excluir', component:ExcluirComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarComponent } from './listar/listar.component';
+import { DetalharComponent } from './detalhar/detalhar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { ExcluirComponent } from './excluir/excluir.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarComponent,
+    DetalharComponent,
+    CadastrarComponent,
+    ExcluirComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
